392e0dd69dc6b2900c327081800ecf8db1271cfd
lgf <lgf_136@163.com>
2022-09-27 23:35:27 +0800

    feat: change file format

--- eslint/react-ts.config.js
+++ eslint/react-ts.config.js
@@ -1,20 +1,19 @@
 module.exports = {
   extends: [
-    './typescript.config',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:react-hooks/recommended',
+    "./typescript.config",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
   ],
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
   },
   rules: {
-    'no-debugger': 'off',
+    "no-debugger": "off",
     // 解决 error 'count' is already declared in the upper scope on line 6 column 10 no-shadow
-    'no-shadow': 'off',
-    'react/prop-types': 'off',
+    "no-shadow": "off",
+    "react/prop-types": "off",
   },
 };
-
