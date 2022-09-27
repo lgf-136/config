5aba1501b117a38b7dbfa3353514a9bc2582cecd
lgf <lgf_136@163.com>
2022-09-27 21:17:22 +0800

    add base.config for srtlelint

---
+++ stylelint/base.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-standard-scss",
+    "stylelint-config-recess-order",
+  ],
+  overrides: [
+    {
+      files: ["**/*.{vue,html}"],
+      customSyntax: "postcss-html",
+    },
+    {
+      files: ["**/*.scss"],
+      customSyntax: "postcss-scss",
+    },
+    {
+      files: ["**/*.less"],
+      customSyntax: "postcss-less",
+    },
+  ],
+  rules: {},
+};
+
+// {
+//   "extends": [
+//     "stylelint-config-standard",
+//     "stylelint-config-standard-scss",
+//     "stylelint-config-recess-order"
+//   ],
+//   "overrides": [
+//     {
+//       "files": [
+//         "src/**/*.scss"
+//       ],
+//       "customSyntax": "postcss-scss"
+//     },
+//     {
+//       "files": [
+//         "src/**/*.less"
+//       ],
+//       "customSyntax": "postcss-less"
+//     },
+//     {
+//       "files": [
+//         "src/**/*.vue"
+//       ],
+//       "customSyntax": "postcss-html"
+//     }
+//   ]
+// }
