097e40851f9273ecdd7f55f40bd62eab46a312c5
lgf <lgf_136@163.com>
2022-09-28 22:27:47 +0800

    fix: add index.ts

--- index.js
+++
@@ -1,3 +0,0 @@
-import reactTsTamperMonkeyConfig from './tampermonkey/react-ts';
-import reactTamperMonkeyConfig from './tampermonkey/react';
-export { reactTsTamperMonkeyConfig, reactTamperMonkeyConfig };
